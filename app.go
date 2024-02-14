0a5a997e270d0cf05da6375617744257de889648
admin <js.byeon@teamsparta.co>
2024-02-14 18:27:45 +0900

    add receiver

--- app.go
+++ app.go
@@ -4,14 +4,20 @@ import (
 	"changeme/protodef"
 	"context"
 	"fmt"
+	"io"
 	"log/slog"
 	"net"
+	"net/http"
+	"strconv"
 	"time"
 
 	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
-
+const (
+	SERVER_IP string = "127.0.0.1"
+	SERVER_PORT int = 8888
+)
 type ClientStatus struct {
 	Id    int32
 	X     int32
@@ -40,6 +46,68 @@ func (a *App) Greet(name string) string {
 	return fmt.Sprintf("Hello %s, It's show time!", name)
 }
 
+// 사실
+func (a *App) LogIn(userId string) int {
+		// 아무 빈 포트에 할당한다.
+		addr, err := net.ResolveUDPAddr("udp", ":0")
+	
+		if err != nil {
+			slog.Debug(err.Error())
+			panic(err)
+		}
+	
+		conn, err := net.ListenUDP("udp", addr)
+	
+		if err != nil {
+			slog.Debug(err.Error())
+			panic(err)
+		}
+
+		resp, err := http.Get("https://ipinfo.io/ip")
+
+		if err != nil {
+			slog.Debug(err.Error())
+			panic(err)
+		}
+
+		defer resp.Body.Close()
+
+		byteIp, err := io.ReadAll(resp.Body)
+
+		if err != nil {
+			slog.Debug(err.Error())
+			panic(err)
+		}
+
+		ip := string(byteIp)
+		clientPort := conn.LocalAddr().(*net.UDPAddr).Port
+
+		workerResp, err := http.Get(fmt.Sprintf("%s/get-worker-port/%s/%s/%d",SERVER_IP, userId, ip, clientPort))
+		
+		if err != nil {
+			slog.Debug(err.Error())
+			panic(err)
+		}
+
+		defer workerResp.Body.Close()
+
+		workerPort, err := io.ReadAll(workerResp.Body)
+
+		if err != nil {
+			slog.Debug(err.Error())
+			panic(err)
+		}
+
+		port, err := strconv.Atoi(string(workerPort))
+		
+		if err != nil {
+			slog.Debug(err.Error())
+			panic(err)
+		}
+
+		return port
+}
+
 func (a *App) SendStatus(clientStatus ClientStatus) {
 	status := protodef.Status{
 		Id:     clientStatus.Id,
@@ -56,7 +124,7 @@ func (a *App) SendStatus(clientStatus ClientStatus) {
 		panic(err)
 	}
 
-	serverAddr, err := net.ResolveUDPAddr("udp", ":8888")
+	serverAddr, err := net.ResolveUDPAddr("udp", fmt.Sprintf("%s:%d", SERVER_IP, SERVER_PORT))
 
 	if err != nil {
 		slog.Debug(err.Error())
